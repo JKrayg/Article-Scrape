3de3c9938a5b27caf6f314ad08446718895901e7
Jake Krayger <jkrayg@protonmail.com>
2020-06-17 08:15:08 -0400

    complete article model

---
+++ models/Article.js
@@ -0,0 +1,28 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    photoUrl: {
+        type: String,
+    },
+
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: "comment"
+    }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
