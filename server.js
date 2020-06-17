dd37eed36c5e1a09e7a5bb8ec852beec913d30af
Jake Krayger <jkrayg@protonmail.com>
2020-06-17 08:51:40 -0400

    model exports (index)

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var mongoose = require("mongoose");
 var logger = require("morgan");
 var axios = require("axios");
 var cheerio = require("cheerio");
-
 var db = require("./models");
 
 var PORT = 3000;
@@ -11,6 +10,7 @@ var PORT = 3000;
 var app = express();
 
 app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -43,5 +43,5 @@ mongoose.connect(MONGODB_URI);
 
 
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
+    console.log("App running on port " + PORT);
   });
